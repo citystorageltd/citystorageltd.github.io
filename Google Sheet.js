0637db3094dfd1bbbf7a712988c8b696a61294af
citystorageltd <hrcitystorage@gmail.com>
2024-07-11 08:22:53 +0400

    Update Google Sheet.js

--- Google Sheet.js
+++ Google Sheet.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxYika_k6TSC8Z8JXhbUQb4wpQD2iAoIWRUfux_q59btazGFFEB_QgvjSBaVhulJh3fPQ/exec'
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx_14f0X0NKpqYxfE4bUD7Vop9mHnX7imE1ioqnEl5O7juSzIqD3o1_XQGduIsZ-3gqaQ/exec'
 
 const form = document.forms['contact-form']
 
